762ebe7a563a3d8e5dba38be889e17df95a0cd7b
Dian Vasilev <92683949+didov02@users.noreply.github.com>
2022-03-14 15:13:58 +0200

    Create Task1.cpp

---
+++ Data Stream and Files/Seminar/Task1.cpp
@@ -0,0 +1,29 @@
+//Напишете програма, която отпечатва собствения си код.
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const int MAX_SIZE = 1024;
+
+int main()
+{
+	ifstream file;
+	file.open("c++.cpp");
+
+	if (!file.is_open())
+	{
+		cout << "Error!" << endl;
+		return 1;
+	}
+
+	while (!file.eof())
+	{
+		char buffer[MAX_SIZE];
+		file.getline(buffer, MAX_SIZE);
+		cout << buffer << endl;
+	}
+
+	return 0;
+}
