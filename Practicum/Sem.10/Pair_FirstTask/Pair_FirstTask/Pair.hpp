76b3042aae0085307f50f00713a6671cc9037ac1
Dian Vasilev <dian.vasilev02@gmail.com>
2022-07-01 15:18:09 +0300

    update

--- Practicum/Sem.10/Pair_FirstTask/Pair_FirstTask/Pair.hpp
+++ Practicum/Sem.10/Pair_FirstTask/Pair_FirstTask/Pair.hpp
@@ -7,58 +7,75 @@ private:
 	T firstElement;
 	T secondElement;
 public:
+	Pair() = default;
+	Pair(const T&, const T&);
 
-	bool operator==(const Pair& other)
-	{
-		if (this->firstElement == other.firstElement
-			&& this->secondElement == other.secondElement)
-		{
-			return true;
-		}
+	bool operator==(const Pair&);
+	bool operator!=(const Pair&);
 
-		return false;
-	}
+	T getFirstElement() const;
+	T getSecondElement() const;
 
-	bool operator!=(const Pair& other)
-	{
-		if (this->firstElement != other.firstElement
-			&& this->secondElement != other.secondElement)
-		{
-			return true;
-		}
+	void setFirstElement(const T&);
+	void setSecondElement(const T&);
 
-		return false;
-	}
-
-
-	T getFirstElement() const
-	{
-		return firstElement;
-	}
-
-	T getSecondElement() const
-	{
-		return secondElement;
-	}
-
-	void setFirstElement(const T& element)
-	{
-		this->firstElement = element;
-	}
-
-	void setSecondElement(const T& element)
-	{
-		this->secondElement = element;
-	}
-
-	const T& getMaxElement() const
-	{
-		if (this->firstElement > this->secondElement)
-		{
-			return this->firstElement;
-		}
-
-		return this->secondElement;
-	}
-
+	T getMaxElement() const;
 };
+
+template<typename T>
+Pair<T>::Pair(const T& firstElement, const T& secondElement)
+{
+	this->firstElement = firstElement;
+	this->secondElement = secondElement;
+}
+
+template<typename T>
+bool Pair<T>::operator==(const Pair& other)
+{
+	if (firstElement == other.firstElement && secondElement == other.secondElement)
+		return true;
+
+	return false;
+}
+
+template<typename T>
+bool Pair<T>::operator!=(const Pair& other)
+{
+	if (firstElement == other.firstElement && secondElement == other.secondElement)
+		return false;
+
+	return true;
+}
+
+template<typename T>
+T Pair<T>::getFirstElement() const
+{
+	return firstElement;
+}
+
+template<typename T>
+T Pair<T>::getSecondElement() const
+{
+	return secondElement;
+}
+
+template<typename T>
+void Pair<T>::setFirstElement(const T& element)
+{
+	firstElement = element;
+}
+
+template<typename T>
+void Pair<T>::setSecondElement(const T& element)
+{
+	secondElement = element;
+}
+
+template<typename T>
+T Pair<T>::getMaxElement() const
+{
+	if (firstElement > secondElement)
+		return firstElement;
+
+	return secondElement;
+}
